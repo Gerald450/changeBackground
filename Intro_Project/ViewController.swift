2e297a0e1cdf594bb866d279e9e6d718dc68e1f2
Gerald Shimo <shimogerald450@gmail.com>
2025-07-12 22:56:15 -0500

    change opacity to 1

--- Intro_Project/ViewController.swift
+++ Intro_Project/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
             let green = CGFloat.random(in: 0...1)
             let blue = CGFloat.random(in: 0...1)
             
-            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+            return UIColor(red: red, green: green, blue: blue, alpha: 1)
         }
         let randomColor = changeColor()
         view.backgroundColor = randomColor
